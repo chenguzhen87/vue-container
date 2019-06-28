5115eeedb60211dcc23759e59bf73d3c75593ab7
icony/精武陈真 <chen_zhen@hisuntech.com>
2019-06-28 19:49:08 +0800

    'Vuex增加非默认导出'

--- vuex.js
+++ vuex.js
@@ -1,3 +1,4 @@
 
-import Vuex from 'vuex'
-export default Vuex;
+import Vuex,{ Store, install, mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from 'vuex'
+export default Vuex
+export { Store, install, mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } 
